5eb0ad9dbe5a9daba6febebb509d6b846d8d0329
Karen Payne <paynekaren@comcast.net>
2022-05-28 18:06:27 -0700

    clean-up

--- EntityFrameworkCoreHasConversion/Classes/PeopleOperations.cs
+++ EntityFrameworkCoreHasConversion/Classes/PeopleOperations.cs
@@ -11,16 +11,20 @@ namespace HasConversion.Classes
         /// Example for <see cref="BoolToStringConverter"/> where a bool is saved to a
         /// column of type nvarchar(3) and read back as a bool.
         /// </summary>
-        public static void AddPeople()
+        public static void AddViewPeople()
         {
             using var context = new PeopleContext();
             
             context.Database.EnsureDeleted();
             context.Database.EnsureCreated();
 
-            context.Add(new Person() { FirstName = "Jim", LastName = "Jacobe", IsFriend = true, DateTime = new DateTime(2022, 5,5)});
+            context.Add(new Person() { FirstName = "Jim", LastName = "Jacobe", IsFriend = true, 
+                DateTime = new DateTime(2022, 5,5)});
+
             context.Add(new Person() { FirstName = "Bob", LastName = "Smith", IsFriend = false });
+
             context.Add(new Person() { FirstName = "Karen", LastName = "Payne", IsFriend = true });
+
             context.SaveChanges();
         }
     }
