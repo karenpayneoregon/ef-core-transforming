b6f3926ad80133f3a2c423ebc3a5bf2929134663
Karen Payne <paynekaren@comcast.net>
2022-05-31 13:33:30 -0700

    removed comment

--- EntityFrameworkApp/Data/SomeContext.cs
+++ EntityFrameworkApp/Data/SomeContext.cs
@@ -9,7 +9,6 @@ namespace EntityFrameworkApp.Data
 {
     public class SomeContext : DbContext
     {
-        // Hello Karen
         public DbSet<SomeEntity> SomeEntities { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
