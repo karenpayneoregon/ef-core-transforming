adb71fe29403bb49d3ba31c2862fb980b74dfc51
Karen Payne <paynekaren@comcast.net>
2022-12-19 11:05:53 -0800

    finish upgrades

--- Console1/Classes/Program.cs
+++ Console1/Classes/Program.cs
@@ -1,21 +1,16 @@
-﻿using System;
+﻿using ConsoleHelperLibrary.Classes;
 using System.Runtime.CompilerServices;
 
 
 // ReSharper disable once CheckNamespace
-namespace Console1
+namespace Console1;
+
+partial class Program
 {
-    partial class Program
+    [ModuleInitializer]
+    public static void Init()
     {
-        [ModuleInitializer]
-        public static void Init()
-        {
-            Console.Title = "Code sample";
-        }
+        Console.Title = "Code sample: AM/PM";
+        WindowUtility.SetConsoleWindowPosition(WindowUtility.AnchorWindow.Center);
     }
 }
-
-
-
-
-
