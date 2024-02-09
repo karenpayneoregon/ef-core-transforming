dac8e66c824466503ce1562c7b3afeb94d1c3235
Karen Payne <paynekaren@comcast.net>
2024-02-09 08:19:31 -0800

    new project

---
+++ SpectreColorsApp/Classes/SpectreItem.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System.Text.Json;
+
+namespace SpectreColorsApp.Classes;
+
+    public class SpectreItem
+    {
+        public byte R { get; set; }
+        public byte G { get; set; }
+        public byte B { get; set; }
+    }
+
+    public class SpectreItemConverter : ValueConverter<SpectreItem, string>
+    {
+        public SpectreItemConverter() : base(
+            v => 
+                JsonSerializer.Serialize(v, (JsonSerializerOptions)null!),
+            v => 
+                JsonSerializer.Deserialize<SpectreItem>(v, (JsonSerializerOptions)null!)!) { }
+    }
+
---
+++ SpectreColorsApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using SpectreColorsApp.Classes;
+using SpectreColorsApp.Data;
+using SpectreColorsApp.Models;
+
+namespace SpectreColorsApp;
+
+internal partial class Program
+{
+    static void Main(string[] args)
+    {
+        using var context = new Context();
+        var list = context.SpectreTable.ToList();
+        foreach (var row in list)
+        {
+            AnsiConsole.MarkupLine($"[{row.Color}]{row.Purpose}[/]");
+        }
+
+        Console.ReadLine();
+    }
+
+    private static void DisplayDemo()
+    {
+        using var context = new Context();
+        var st = context.SpectreTable.FirstOrDefault( x=> x.Purpose == "Main text");
+
+        var color = new Color(st.ItemColor.R, st.ItemColor.G, st.ItemColor.B);
+        
+        AnsiConsole.MarkupLine($"[{color.ToMarkup()}]Test[/]");
+    }
+
+    private static void CreateRecord()
+    {
+        SpectreTable spectreTable = new SpectreTable()
+        {
+            Purpose = "Secondary",
+            ItemColor = new SpectreItem() { R = 255, G = 255, B = 0 }
+        };
+
+        using var context = new Context();
+        context.SpectreTable.Add(spectreTable);
+        Console.WriteLine(context.SaveChanges());
+    }
+}
