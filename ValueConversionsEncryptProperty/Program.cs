d3941a0fa0dacf00cd661d50afd05b1a48d99cbb
Karen Payne <paynekaren@comcast.net>
2025-01-15 04:29:35 -0800

    Update target framework to .NET 9 and improve password verification output formatting

--- ValueConversionsEncryptProperty/Program.cs
+++ ValueConversionsEncryptProperty/Program.cs
@@ -31,11 +31,14 @@ internal partial class Program
             AnsiConsole.MarkupLine($"User [cyan]{user.Name}[/] has password [cyan]'{user.Password}'[/]");
             
             var verified = BC.Verify("password", user.Password);
-            AnsiConsole.MarkupLine(verified ? "[green]Password is correct[/]" : "[red]Password is incorrect[/]");
+            AnsiConsole.MarkupLine(verified ? 
+                "[green]Password is correct[/]" : 
+                "[red]Password is incorrect[/]");
 
             verified = BC.Verify("password1", user.Password);
             Log.Information("User {@user} password from database {@password}", user.Name, user.Password);
             AnsiConsole.MarkupLine(verified ? "[green]Password is correct[/]" : "[red]Password is incorrect[/]");
+
         }
 
         Console.ReadLine();
